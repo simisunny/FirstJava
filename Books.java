4c205a497f6e63754915e60edc88a472be667401
Santwana <34178116+simisunny@users.noreply.github.com>
2023-03-16 23:30:41 +0530

    Add files via upload

---
+++ Books.java
@@ -0,0 +1,18 @@
+public class Books
+{
+private String name;
+public String category;
+public int price;
+
+public Books()
+{
+
+}
+public Books(String name, String category, int price)
+{
+this.name=name;
+this.category=category;
+this.price=price;
+}
+}
+
